95014577ed67f424cc1679dee22d881c92410dd7
lianaprtw <liana.pratiwi15@gmail.com>
2025-06-20 05:03:33 +0800

    menambahkan file user.js di folder type

--- backend_health_tracker/type/user.js
+++ backend_health_tracker/type/user.js
@@ -1,6 +1,11 @@
-import { gql } from 'graphql-tag';
+// mutationDefs.js
+import { gql } from "graphql-tag";
 
 export const userType = gql`
+  type ForgotPasswordResponse {
+    success: Boolean!
+    message: String!
+  }
   type User {
     id_user: Int
     username: String
@@ -10,5 +15,20 @@ export const userType = gql`
     gander: String
     tinggi_badan: Float
     berat_badan: Float
+  }
+
+  type Mutation {
+    addUser(
+      username: String!
+      gmail: String!
+      password: String!
+      umur: Int!
+      gander: String!
+      tinggi_badan: Float!
+      berat_badan: Float!
+      profile: String
+    ): User
+
+    forgotPassword(gmail: String!, newPassword: String!): ForgotPasswordResponse
   }
 `;
