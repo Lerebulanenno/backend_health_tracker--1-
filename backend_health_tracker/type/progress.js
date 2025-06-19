967635a98ef9da177ffdc14b3d947015a40c0c71
Enno Lerebulan <lerebulanenno@gmail.com>
2025-06-20 05:17:40 +0800

    Menambahkan & Update file type Progress

--- backend_health_tracker/type/progress.js
+++ backend_health_tracker/type/progress.js
@@ -1,10 +1,10 @@
-import { gql } from 'graphql-tag';
+import { gql } from "graphql-tag";
 
 export const progressType = gql`
   type Progress {
     id_progress: Int
     id_user: Int
-    id_trainner: Int
+
     drink: Float
     jarak: Float
     duration: String
@@ -12,6 +12,5 @@ export const progressType = gql`
     jenis_kegiatan: String
     waktu: String
     user: User
-    trainner: Trainner
   }
 `;
